6f5474c27941b16bb56387240fab043a9147c8cb
DanPeled <DanPeled@users.noreply.github.com>
2025-07-27 08:37:20 +0000

    deploy: 46541f83680637e35ba9cdfee08a2200caff7840

--- search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['wpilib_20license_0',['WPILib-License',['../md_WPILib-License.html',1,'']]]
-];
--- search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['license_0',['license',['../md_LICENSE.html',1,'LICENSE'],['../md_WPILib-License.html',1,'WPILib-License']]]
-];
